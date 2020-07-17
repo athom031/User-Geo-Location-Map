c944da20b06fb69e545f593f46cc04f5edff4f83
Alex Thomas <alexjosthomas@gmail.com>
2020-07-17 14:30:16 -0700

    pending search feature doesnt work, added placeholder text

--- my-app/src/components/Form/form.jsx
+++ my-app/src/components/Form/form.jsx
@@ -16,61 +16,40 @@ export class Form extends React.Component {
             address:      '',
             password:     '',
             confPassword: '',
-            regSuccess: false,
-            pendingSearch: false
             //feature that while searching for something dont do anything for register
         }
     }
     
     //Form Controlled Components    
     //when handler is assigned to on change event, event itself is passed as param    
-    handleFullnameChange = event => {
-        this.setState({
-            fullName: event.target.value 
-        })
-    }
-    handleUsernameChange = event => {
-        this.setState({ 
-            userName: event.target.value
-        })
-    }
-    handleAddressChange = event => {
-        this.setState({
-            address: event.target.value
-        })
-    }
-    handlePasswordChange = event => {
-        this.setState({
-            password: event.target.value
-        })
-    }
-    handleConfpasswordChange = event => {
-        this.setState({
-            confPassword: event.target.value
-        })
-    }
+    handleFullnameChange = event => { this.setState({ fullName: event.target.value }) }
+    
+    handleUsernameChange = event => { this.setState({ userName: event.target.value }) }
+    
+    handleAddressChange = event => { this.setState({ address: event.target.value }) }
+    
+    handlePasswordChange = event => { this.setState({ password: event.target.value }) }
+    
+    handleConfpasswordChange = event => { this.setState({ confPassword: event.target.value }) }
 
     handleSubmit = event => {
-        if(!this.state.pendingSearch) {
-            this.state.pendingSearch = true;
-
-            var data = JSON.stringify({"fullName": this.state.fullName,
-                                   "userName": this.state.userName,
-                                   "address":  this.state.address,
-                                   "password": this.state.password,
-                                   "confPassword": this.state.confPassword
-                                  });
-            if(checkData(data)) {
-                //check data for matching passwords, address is in the us, password constraints
-                submitHelper(this, data);
-                event.preventDefault();
-                //if successfully sends data refresh page -> potentially after a success message?
-                //username being in db to be handled on server side
-            } else {
-            event.preventDefault(); //stops page from refreshing which we will take out after handling success/error
-            }
-            this.state.pendingSearch = false;
+    
+        var data = JSON.stringify({"fullName": this.state.fullName,
+                                "userName": this.state.userName,
+                                "address":  this.state.address,
+                                "password": this.state.password,
+                                "confPassword": this.state.confPassword
+                                });
+        if(checkData(data)) {
+            //check data for matching passwords, address is in the us, password constraints
+            submitHelper(this, data);
+            event.preventDefault();
+            //if successfully sends data refresh page -> potentially after a success message?
+            //username being in db to be handled on server side
+        } else {
+        event.preventDefault(); //stops page from refreshing which we will take out after handling success/error
         }
+        
         event.preventDefault();
     }
 
@@ -82,7 +61,6 @@ export class Form extends React.Component {
             address:      '',
             password:     '',
             confPassword: '',
-            regSuccess: false,
             pendingSearch: false
         })
     }
@@ -107,12 +85,12 @@ export class Form extends React.Component {
                                 {fullName}<br/>{userName}
                             </div>
                         </div>
-                        <button type="button" onClick={this.regAgain} className="btn">Register Another User</button>
+                        <button type="button" onClick={this.regAgain} className="btn">New Registration</button>
                     </div>
                 </div>
                 :
                 <div className="reg-container">
-                    <div className="header">Register</div>
+                    <div className="header">Register Account</div>
                     <div className="content">
                         <div className="image">
                             <img src={user}/>
@@ -121,23 +99,23 @@ export class Form extends React.Component {
                             <form onSubmit={this.handleSubmit}>
                                 <div className="form-group">
                                     <label>Full Name</label>
-                                    <input type='text' value = { fullName }  onChange = { this.handleFullnameChange } required />
+                                    <input type='text' value = { fullName }  onChange = { this.handleFullnameChange } placeholder='First and Last Name' required />
                                 </div>
                                 <div className="form-group">
                                     <label>Username</label>
-                                    <input type='text' value = { userName }  onChange = { this.handleUsernameChange } required />
+                                    <input type='text' value = { userName }  onChange = { this.handleUsernameChange } placeholder='Unique Username' required />
                                 </div>
                                 <div className="form-group">
                                     <label>Address</label>
-                                    <input type='text' value = { address }  onChange = { this.handleAddressChange } required />
+                                    <input type='text' value = { address }  onChange = { this.handleAddressChange } placeholder = 'U.S. Address' required />
                                 </div>
                                 <div className="form-group">
                                     <label>Password</label>
-                                    <input type='password' value = { password }  onChange = { this.handlePasswordChange } required />
+                                    <input type='password' value = { password }  onChange = { this.handlePasswordChange } placeholder = 'Min-length: 6, 1 Capital, 1 Special' />
                                 </div>
                                 <div className="form-group">
                                     <label>Confirm Password</label>
-                                    <input type='password' value = { confPassword }  onChange = { this.handleConfpasswordChange } required />
+                                    <input type='password' value = { confPassword }  onChange = { this.handleConfpasswordChange } placeholder = 'Must match password' required />
                                 </div>
                                 <div className="footer">
                                     <button type="submit" className = "btn">
